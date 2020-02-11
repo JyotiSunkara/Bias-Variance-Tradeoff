8e7660129a6c4ab67cd0206d694862385773157d
JyotiSunkara <jyoti.sunkara@students.iiit.ac.in>
2020-02-12 00:36:40 +0530

    Our report is gonna be 30 pages long hai ram

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Bias-Variance-Tradeoff
+
  Calculation of the bias and variance after training two models.
+
+ ## Getting Started
+
+ ## Task 0
+ 
+ - 5000 entries
+ - 90:10 split of data as training set and testing set done once
+ 
+- [ ] Training set split into 10 sets to create 10 different models for each eqaution
+- [ ] Bias-Variance tradeoffs of the models analysed using Bias<sup>2</sup> versus Variance graphs and bulls eye graphs.
+
+
+ ## Task 1
+
--- playaround.py
+++ playaround.py
@@ -1,4 +1,7 @@
 import pickle
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import numpy
 
 file = open('Assignment/Q1_data/data.pkl', 'rb')
 data =  pickle.load(file)
@@ -10,3 +13,30 @@ for item in data:
     count = count + 1
 
 print("There are ", count, "entries!")
+
+print(data.shape)
+
+X = data[:,0]
+Y = data[:,1]
+
+for item in X:
+    print(item)
+
+for item in Y:
+    print(item)
+
+print(X.shape)
+print(Y.shape)
+
+
+# Partitioning into training and testing sets 
+# X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.1) 
+
+# # Training y = mx
+# linearRegressor = LinearRegression()  
+# linearRegressor.fit(X_train, Y_train)
+
+
+
+
+
