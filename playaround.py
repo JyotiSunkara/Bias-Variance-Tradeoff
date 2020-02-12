8d239ccb48ebac7d5b545556ebe930f84c8dc56e
Shradha <shradha.sgl@gmail.com>
2020-02-12 15:31:07 +0530

    Models obtained

--- playaround.py
+++ playaround.py
@@ -3,12 +3,14 @@ import matplotlib.pyplot as plot
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import numpy as np
+from sklearn.preprocessing import PolynomialFeatures
 
 file = open('Assignment/Q1_data/data.pkl', 'rb')
 data =  pickle.load(file)
 file.close()
 
 X = data[:, 0]
+# X = X[:,np.newaxis]
 Y = data[:, 1]
 
 # Partitioning into training and testing sets 
@@ -27,20 +29,46 @@ Y_train_split = np.array_split(Y_train, 10)
 
 print(len(X_train_split), len(X_train_split[0]))
 
-# Training y = mx
+# Training polynomials
 linearRegressor = LinearRegression()
-i = 0
 
-while i < 10:  
+j = 1  
+while j < 10: # Choosing training subset 
+
+    i = 1
+    while i < 11: # Choosing polynomial power
+
+        poly = PolynomialFeatures(i)
+        X_poly = poly.fit_transform(X_train_split[j])
+        print(X_poly.shape)
+        linearRegressor.fit(X_poly, Y_train_split[j])        
+        plot.scatter(X_train_split[j], Y_train_split[j], color = 'red')
+        plot.scatter(X_train_split[j], linearRegressor.predict(X_poly), color = 'blue')
+        plot.title('X vs Y (Training set)')
+        plot.xlabel('X')
+        plot.ylabel('Y')
+        plot.show()
+        i += 1
+
+        # X_test_poly = poly.fit_transform(X_test)
+        # Outputs = linearRegressor.predict(X_test_poly)
+        # # bias = np.mean( (Outputs - Y_test) ** 2)
+        # var = np.var(Outputs)
+
+        # # plot.scatter(X_test, bias, color = 'red')
+        # plot.scatter(X_test, var, color = 'blue')
+        # plot.title('bias & var')
+        # plot.xlabel(j)
+        # plot.ylabel('Y')
     
-    linearRegressor.fit(X_train_split[i], Y_train_split[i])
-    plot.scatter(X_train_split[i], Y_train_split[i], color = 'red')
-    plot.plot(X_train_split[i], linearRegressor.predict(X_train_split[i]), color = 'blue')
-    plot.title('X vs Y (Training set)')
-    plot.xlabel('X')
-    plot.ylabel('Y')
-    plot.show()
-    i += 1
+    j += 1
+    #plot.show()
+
+
+
+
+    
+
 
 
 
