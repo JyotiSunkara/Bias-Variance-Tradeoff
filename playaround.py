9998fd10f8f531b46e981cddf1cef6bb55d87cbd
Shradha <shradha.sgl@gmail.com>
2020-02-12 11:44:04 +0530

    Trained linear model on different sets

--- playaround.py
+++ playaround.py
@@ -8,55 +8,39 @@ file = open('Assignment/Q1_data/data.pkl', 'rb')
 data =  pickle.load(file)
 file.close()
 
-# count = 0 
-# for item in data:
-#     print(item)
-#     count = count + 1
-
-# print("There are ", count, "entries!")
-
-print(data.shape)
-
-
 X = data[:, 0]
 Y = data[:, 1]
-
-# print(X.shape)
-# print(Y.shape)
 
 # Partitioning into training and testing sets 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.1) 
 
-# Partition into 10 training sets to obtain 10 models
-
+# Reshuffling data
 training_data = np.column_stack((X_train, Y_train))
 np.random.shuffle(training_data)
 X_train = training_data[:, 0]
+X_train = X_train[:, np.newaxis]
 Y_train = training_data[:, 1]
 
-
-# training_data_split = np.array_split(training_data, 10)
-# print(training_data_split)
-
+# Splitting training data into 10 parts
 X_train_split = np.array_split(X_train, 10)
 Y_train_split = np.array_split(Y_train, 10)
 
 print(len(X_train_split), len(X_train_split[0]))
 
-# np.array_split(x, 3)
-# print(X_new)
-# print(Y_train.shape)
 # Training y = mx
-# linearRegressor = LinearRegression()  
-# linearRegressor.fit(X_train[:,np.newaxis],Y_train)
+linearRegressor = LinearRegression()
+i = 0
 
-# plot.scatter(X_train, Y_train, color = 'red')
-# plot.plot(X_train, linearRegressor.predict(X_train[:,numpy.newaxis]), color = 'blue')
-# plot.title('X vs Y (Training set)')
-# plot.xlabel('X')
-# plot.ylabel('Y')
-# plot.show()
-
+while i < 10:  
+    
+    linearRegressor.fit(X_train_split[i], Y_train_split[i])
+    plot.scatter(X_train_split[i], Y_train_split[i], color = 'red')
+    plot.plot(X_train_split[i], linearRegressor.predict(X_train_split[i]), color = 'blue')
+    plot.title('X vs Y (Training set)')
+    plot.xlabel('X')
+    plot.ylabel('Y')
+    plot.show()
+    i += 1
 
 
 
