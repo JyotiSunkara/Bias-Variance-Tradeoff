7476d5d5e5eebc6808fc103667d3c03a40ddd09a
JyotiSunkara <jyoti.sunkara@students.iiit.ac.in>
2020-02-12 08:59:50 +0530

    Playing around

--- README.md
+++ README.md
@@ -10,7 +10,15 @@
  - 90:10 split of data as training set and testing set done once
  
 - [ ] Training set split into 10 sets to create 10 different models for each eqaution
-- [ ] Bias-Variance tradeoffs of the models analysed using Bias<sup>2</sup> versus Variance graphs and bulls eye graphs.
+- [ ] Bias-Variance tradeoffs of the models analysed using Bias<sup>2</sup> Vs Variance graphs.
+- [ ] Bulls eye graphs.
+
+### y = mx
+### y = ax<sup>2</sup> + bx + c
+### y = ax<sup>3</sup>  + bx<sup>2</sup> + cx + d
+### y = ax<sup>4</sup>  + bx<sup>3</sup> + cx<sup>2</sup> + dx + e
+### y = ax<sup>5</sup>  + bx<sup>4</sup> + cx<sup>3</sup> + dx<sup>2</sup> + ex + f
+### y = ax<sup>6</sup>  + bx<sup>5</sup> + cx<sup>4</sup> + dx<sup>3</sup> + ex<sup>2</sup> + fx + g
 
 
  ## Task 1
--- playaround.py
+++ playaround.py
@@ -1,4 +1,5 @@
 import pickle
+import matplotlib.pyplot as plot
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import numpy
@@ -16,25 +17,36 @@ print("There are ", count, "entries!")
 
 print(data.shape)
 
-X = data[:,0]
-Y = data[:,1]
 
-for item in X:
-    print(item)
+X = data[:, 0]
+Y = data[:, 1]
 
-for item in Y:
-    print(item)
 
-print(X.shape)
-print(Y.shape)
+# for item in X:
+#     print(item)
 
+# for item in Y:
+#     print(item)
+
+# print(X.shape)
+# print(Y.shape)
 
 # Partitioning into training and testing sets 
-# X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.1) 
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.1) 
 
-# # Training y = mx
-# linearRegressor = LinearRegression()  
-# linearRegressor.fit(X_train, Y_train)
+# Partition into 10 training sets to obtain 10 models 
+
+# Training y = mx
+linearRegressor = LinearRegression()  
+linearRegressor.fit(X_train[:,numpy.newaxis],Y_train)
+
+plot.scatter(X_train, Y_train, color = 'red')
+plot.plot(X_train, linearRegressor.predict(X_train[:,numpy.newaxis]), color = 'blue')
+plot.title('X vs Y (Training set)')
+plot.xlabel('X')
+plot.ylabel('Y')
+plot.show()
+
 
 
 
